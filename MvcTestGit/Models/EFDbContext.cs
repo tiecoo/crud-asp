cdfbc2c10c27878c23b84c827fb7b023e3aa0661
tiecoo <diego.pereirasilva@live.com>
2016-05-16 22:46:00 -0300

    Db created

---
+++ MvcTestGit/Models/EFDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcTestGit.Models
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Product> Produtos { get; set; }
+    }
+}
